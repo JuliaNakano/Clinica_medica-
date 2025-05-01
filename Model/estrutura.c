df9bbca1529a4f5ee93d941ece844c62333c4a15
JuliaNakano <jumartinsnakano@gmail.com>
2025-05-01 15:07:39 -0300

    Colocando as structs

---
+++ Model/estrutura.c
@@ -0,0 +1,95 @@
+//As struct aqui declaradas representam os dados a serem manipualdos em cada "tabela"
+// Julia Cristina Martins de Almeida Nakano
+#include<stdio.h>
+
+//Informações sobre endereço
+typedef struct {
+    char rua[100];
+    char bairro[50];
+    char cidade[50];
+    char estado[3];  // Ex: "SP", "MG"
+} Endereco;
+
+// Informações do pacientes
+typedef struct {
+    int codigo;                    // Código único
+    char nomeCompleto[100];       // Nome completo
+    char cpf[15];                 // CPF (formato: 000.000.000-00)
+    
+    Endereco endereco; //Usando o endereço
+    char telefones[2][20];        // Até 32telefones
+    char dataNascimento[11];      // Formato: DD/MM/AAAA
+    char historicoMedico[200];    // Ex: alergias, doenças crônicas
+} Paciente;
+
+//Informações para Profissionais de saúde 
+typedef struct {
+    int codigo;                    // Código único
+    char nomeCompleto[100];       // Nome completo
+    char crm[20];                 // CRM ou registro equivalente
+    char especialidade[50];       // Especialidade médica
+    char cpf[15];                 // CPF (formato: 000.000.000-00)
+    char telefones[2][20];        // Até 2 telefones
+    char email[100];              // E-mail
+} Psaude;
+
+
+//Informações sobre os fornecedores 
+typedef struct {
+    int codigo;                      // Código único
+    char nomeFantasia[100];         // Nome fantasia
+    char razaoSocial[100];          // Razão social
+    char inscricaoEstadual[30];     // Inscrição estadual
+    char cnpj[20];                  // CNPJ (formato: 00.000.000/0000-00)
+    Endereco endereco;              //Usando o endereço
+    char telefones[11] ;          // Até 2 telefones
+    char email[100];                // E-mail
+} Fornecedor;
+
+
+//Ambientes medicos
+typedef struct {
+    int codigo;                      // Código único
+    char descricaoProcedimento[200]; //Descriçãodo procedimento
+} Amedico;
+
+
+//Informações sobre os materiais e medicamentos 
+typedef struct {
+    int codigo;                     // Código único
+    char descricao[100];           // Descrição
+    char fabricante[100];          // Fabricante
+    Fornecedor * fornecedor;        // Fornecedor
+    float precoCusto;              // Preço de custo
+    float precoVenda;              // Preço de venda
+    int quantidadeEstoque;         // Quantidade em estoque
+    int estoqueMinimo;             // Estoque mínimo
+} MaterialMedicamento;
+
+// Informações sobre os procedimentos medicos
+typedef struct {
+    int codigo;                             // Código único
+    char descricaoProcedimento[200];       // Descrição do procedimento
+    float custo;                            // Custo do procedimento
+    char tempoEstimado[30];                // Tempo estimado de realização (ex: "30 minutos")
+    char ambienteMedico[100];              // Ambiente médico (ex: "Sala de cirurgia", "Ambulatório")
+    char materiaisUtilizados[300];         // Medicamentos e materiais utilizados
+} Pmedico;
+
+
+//Testando 
+/*int main() {
+    // Exemplificação de uso
+    Fornecedor fornecedor1 = {1, "Fornecedor A", "Razão Social A", "123456789", "00.000.000/0000-00", 
+                               {"Rua A", "Bairro A", "Cidade A", "SP"}, "123456789", "fornecedorA@example.com"};
+    
+    MaterialMedicamento medicamento1 = {1, "Medicamento X", "Fabricante Y", &fornecedor1, 10.5, 20.0, 100, 10};
+    
+    // Exemplo de acesso aos dados
+    printf("Medicamento: %s\n", medicamento1.descricao);
+    printf("Fornecedor: %s\n", medicamento1.fornecedor->nomeFantasia);
+    
+    return 0;
+}
+*/
+
---
+++ View/menu.c
@@ -0,0 +1,168 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+   int opcao;
+
+    printf("Sistema Clinica Medica\n \n ");
+
+   
+    printf("\n--------------------------------\n");
+    printf("|             MENU                |\n");
+    printf("---------------------------------\n");
+    printf("---------------------------------\n");
+    printf("| |1| Pacientes                  |\n");
+    printf("---------------------------------\n");
+    printf("| |2| Proficionais da Saude      |\n ");
+    printf("---------------------------------\n");
+    printf("| |3| Fornecedores               |\n ");
+    printf("---------------------------------\n");
+    printf("| |4| Ambientes Medicos          |\n ");
+    printf("---------------------------------\n");
+    printf("| |5| Medicamentos e Materiais   |\n ");
+    printf("---------------------------------\n");
+    printf("| |6| Procedimentos Medicos      |\n");
+    printf("---------------------------------\n");
+    printf("---------------------------------\n");
+    printf("| |7| ENCERRAR                  |\n");
+    printf("---------------------------------\n");
+    
+
+
+    // 
+
+    while (opcao!=0){
+        switch (opcao)
+        {
+            case 1:
+            printf("--------- Pacientes ---------\n");
+            printf("-----------------------------\n");
+            printf("| |1| Cadastrar              |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |2| Atualizar              |\n");
+            printf("----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |3| Listar                 |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |4| Deletar                |\n");
+            printf("-----------------------------\n");
+            printf("------------------------------\n");
+            printf("| |5| Mostrar MENU           |\n");
+            printf("------------------------------\n");
+            
+            //Escolhendo a opção para dar segmento no sistema 
+            
+
+                
+            case 2: 
+            printf("-- Proficionais da Saude-----\n");
+            printf("-----------------------------\n");
+            printf("| |1| Cadastrar              |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |2| Atualizar              |\n");
+            printf("----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |3| Listar                 |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |4| Deletar                |\n");
+            printf("-----------------------------\n");
+            printf("------------------------------\n");
+            printf("| |5| Mostrar MENU           |\n");
+            printf("------------------------------\n");
+
+            case 3 :
+
+            printf("---------Fornecedores--------\n");
+            printf("-----------------------------\n");
+            printf("| |1| Cadastrar              |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |2| Atualizar              |\n");
+            printf("----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |3| Listar                 |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |4| Deletar                |\n");
+            printf("-----------------------------\n");
+            printf("------------------------------\n");
+            printf("| |5| Mostrar MENU           |\n");
+            printf("------------------------------\n");
+
+            case 4: 
+            printf("------Ambientes Medicos------\n");
+            printf("-----------------------------\n");
+            printf("| |1| Cadastrar              |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |2| Atualizar              |\n");
+            printf("----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |3| Listar                 |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |4| Deletar                |\n");
+            printf("-----------------------------\n");
+            printf("------------------------------\n");
+            printf("| |5| Mostrar MENU           |\n");
+            printf("------------------------------\n");
+            
+            case 5: 
+
+            printf("--Medicamentos e Materiais---\n");
+            printf("-----------------------------\n");
+            printf("| |1| Cadastrar              |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |2| Atualizar              |\n");
+            printf("----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |3| Listar                 |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |4| Deletar                |\n");
+            printf("-----------------------------\n");
+            printf("------------------------------\n");
+            printf("| |5| Mostrar MENU           |\n");
+            printf("------------------------------\n");
+
+            case 6: 
+            printf("--- Procedimentos Medicos ---\n");
+            printf("-----------------------------\n");
+            printf("| |1| Cadastrar              |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |2| Atualizar              |\n");
+            printf("----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |3| Listar                 |\n");
+            printf("-----------------------------\n");
+            printf("-----------------------------\n");
+            printf("| |4| Deletar                |\n");
+            printf("-----------------------------\n");
+            printf("------------------------------\n");
+            printf("| |5| Mostrar MENU           |\n");
+            printf("------------------------------\n");
+                
+            case 7: 
+            printf("OBRIGADO POR UTILIZAR O SISTEMA CLINICA MEDICA!!!");
+            break;
+            
+        }
+
+       
+        
+    
+    
+    }
+
+ 
+   
+    return 0;
+}
+
